f7fcec04e88a80bfe44a682496eb32ce54ddce1d
Yami69fg <135854840+Yami69fg@users.noreply.github.com>
2024-10-30 23:39:26 +0300

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -7,18 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        
-        self.window?.rootViewController = UINavigationController(rootViewController: CoreLoadingController())
-        
-        self.window?.makeKeyAndVisible()
-        
-        return true
-    }
-    
     
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         
@@ -26,5 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return .portrait
     }
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UINavigationController(rootViewController: CoreLoadingController())
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
 
 }
